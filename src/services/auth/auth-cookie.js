067c99294540520f3f04bfc8c9c064da7a09e023
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-04-29 14:17:02 +0300

    Create auth-cookie.js

---
+++ src/services/auth/auth-cookie.js
@@ -0,0 +1,10 @@
+const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+const setAuthCookie = (res, token) => {
+    res.cookie('auth', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: ONE_DAY_MS
+    })
+};
+
+module.exports = {setAuthCookie};
