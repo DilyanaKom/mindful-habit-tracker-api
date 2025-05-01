effec4d3586271da74cd1e43ede1b97971214b73
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-05-01 15:54:54 +0300

    Habit service wip

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth-routes.js');
+const habitRouter = require('./routes/habit-routes.js');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('/api/habits', habitRouter);
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server is listening on http://localhost:3000...'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log('Server is listening on http://localhost:3001...'));
 
